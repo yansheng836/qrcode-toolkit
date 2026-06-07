e81d876bd2e78fe6b7fd59871b1e95fe1e21ec2f
yansheng836 <sheng.yan836@gmail.com>
2026-06-07 16:57:49 +0800

    feat: 添加工具函数模块

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,43 @@
-// 工具函数
+// js/utils.js
+
+/**
+ * 文件转 Data URL
+ * @param {File} file
+ * @returns {Promise<string>}
+ */
+function fileToDataURL(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
+
+/**
+ * 触发浏览器下载
+ * @param {string} dataURL - Data URL 或 Blob URL
+ * @param {string} filename - 下载文件名
+ */
+function downloadFile(dataURL, filename) {
+  const a = document.createElement('a');
+  a.href = dataURL;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+/**
+ * 防抖函数
+ * @param {Function} fn
+ * @param {number} ms - 延迟毫秒数
+ * @returns {Function}
+ */
+function debounce(fn, ms = 300) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, args), ms);
+  };
+}
